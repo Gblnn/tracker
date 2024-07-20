4d76ab5a65b2776d5fcca71da023da785941daef
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-07-20 09:01:46 +0400

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,20 +4,12 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyDHEK5fSNrTJJczkFMV0h6MmYjPS44cnO0",
-  
+    apiKey: "AIzaSyDHEK5fSNrTJJczkFMV0h6MmYjPS44cnO0",
     authDomain: "ssu-db-ec149.firebaseapp.com",
-  
     projectId: "ssu-db-ec149",
-  
     storageBucket: "ssu-db-ec149.appspot.com",
-  
     messagingSenderId: "358217039306",
-  
     appId: "1:358217039306:web:1c091e2355728ed352a333"
-
-  
-  
   };
 
 // Initialize Firebase
