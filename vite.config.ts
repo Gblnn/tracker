b09118591ed8ce580bcec674b34192044d62d312
Gokul <122973682+Gblnn@users.noreply.github.com>
2025-04-19 09:12:13 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,93 +4,28 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   plugins: [
     react(),
     VitePWA({
-      registerType: "prompt",
-      strategies: "generateSW",
-      injectRegister: "auto",
-      manifest: {
-        name: "PNS Tracker",
-        short_name: "PNS",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
+      registerType: "autoUpdate",
+      includeAssets: "**/*",
       workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,json}"],
-        // runtimeCaching: [
-        //   {
-        //     urlPattern:
-        //       /^https:\/\/firestore\.googleapis\.com\/(?!.*\/Listen\/channel).*/i,
-        //     handler: "NetworkFirst",
-        //     options: {
-        //       cacheName: "firebase-data",
-        //       expiration: {
-        //         maxEntries: 100,
-        //         maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        //       },
-        //       cacheableResponse: {
-        //         statuses: [0, 200],
-        //       },
-        //     },
-        //   },
-        //   {
-        //     urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-        //     handler: "CacheFirst",
-        //     options: {
-        //       cacheName: "google-fonts",
-        //       expiration: {
-        //         maxEntries: 10,
-        //         maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        //       },
-        //     },
-        //   },
-        //   {
-        //     urlPattern: /\.(?:js|css)$/i,
-        //     handler: "StaleWhileRevalidate",
-        //     options: {
-        //       cacheName: "static-resources",
-        //       expiration: {
-        //         maxEntries: 50,
-        //         maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        //       },
-        //     },
-        //   },
-        //   {
-        //     urlPattern: new RegExp("^https://.*"),
-        //     handler: "NetworkFirst",
-        //     options: {
-        //       cacheName: "external-resources",
-        //       networkTimeoutSeconds: 10,
-        //       expiration: {
-        //         maxEntries: 50,
-        //         maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        //       },
-        //       cacheableResponse: {
-        //         statuses: [0, 200],
-        //       },
-        //     },
-        //   },
-        // ],
-        skipWaiting: true,
-        clientsClaim: true,
-        cleanupOutdatedCaches: true,
-        disableDevLogs: true,
-      },
-      devOptions: {
-        enabled: true,
-        type: "module",
+        globPatterns: ["**/*"],
       },
     }),
   ],
