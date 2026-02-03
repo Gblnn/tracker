d59abdc7680c16263e5a0f689114fa5e46f1fb33
Gokul <122973682+Gblnn@users.noreply.github.com>
2026-02-03 15:35:06 +0400

    bug fix

--- src/components/input-dialog.tsx
+++ src/components/input-dialog.tsx
@@ -71,7 +71,7 @@ interface Props{
 
 export default function InputDialog(props:Props){
     return(
-        <Dialog open={props.open} onOpenChange={props.onOpenChange} modal={false}>
+        <Dialog open={props.open} modal={false}>
             <DialogContent onOpenAutoFocus={(e)=>e.preventDefault()}>
                 <DialogHeader>
                     <DialogTitle className="heading" style={{userSelect:"none", width:"100%", display:"flex", flexFlow:"column"}}>
--- src/components/password-dialog.tsx
+++ src/components/password-dialog.tsx
@@ -50,7 +50,7 @@ interface Props{
 
 export default function PasswordDialog(props:Props){
     return(
-        <Dialog open={props.open} onOpenChange={props.onOpenChange} modal={false}>
+        <Dialog open={props.open} modal={false}>
             <DialogContent onOpenAutoFocus={(e)=>e.preventDefault()}>
                 <DialogHeader>
                     <DialogTitle className="heading" style={{userSelect:"none", width:"100%", display:"flex", flexFlow:"column"}}>
