a62e64e1ef6cf3d68dd46286356b25e5e18c8fc1
Gokul <122973682+Gblnn@users.noreply.github.com>
2026-06-08 11:21:48 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -349,7 +349,7 @@ export default function Index() {
           fixed
           editMode={userData?.editor===true? true : false}
             title="Starboard"
-            subtitle={"1.33"}
+            subtitle={"1.34"}
             icon={<img src="/stardox-bg.png" style={{width:"2rem"}} alt="Starboard" />}
             noback
             extra={
