1d877680ea8c5dde075dedcdfb97e09723ef6077
Gokul <122973682+Gblnn@users.noreply.github.com>
2026-06-08 10:59:23 +0400

    update

--- api/attendance.js
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const raw = req.body;
-
-  console.log("Punch:", raw);
-
-  res.status(200).send("OK");
-}
