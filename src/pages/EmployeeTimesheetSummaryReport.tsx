1f41471ce92bcb9a4a0e805857424f73ba6d8f8a
bejoyg-soharstar <bejoy@soharstar.com>
2026-09-08 12:18:49 +0400

    Issue with full report load corrected

--- src/pages/EmployeeTimesheetSummaryReport.tsx
+++ src/pages/EmployeeTimesheetSummaryReport.tsx
@@ -231,6 +231,10 @@ export default function EmployeeTimesheetSummaryReport() {
 
         let query = supabase.from('v_employee_timesheet_summary').select('*').order('date', { ascending: false }).range(offset, offset + PAGE_SIZE - 1);
         if (startDate && endDate) query = query.gte('date', `${startDate}T00:00:00+04:00`).lt('date', `${endDate}T00:00:00+04:00`);
+        if (companyFilter) query = query.eq('company_name', companyFilter);
+        if (projectFilter) query = query.eq('project_code', projectFilter);
+        if (employeeFilter) query = query.eq('name', employeeFilter);
+        if (statusFilter !== 'ALL') query = query.eq('timecard_status', statusFilter);       
         const { data, error } = await query;
         if (error) throw error;
 
@@ -247,7 +251,7 @@ export default function EmployeeTimesheetSummaryReport() {
       setLoading(false);
       setLoadingAll(false);
     }
-  }, [dateFilter, monthFilter]);
+  }, [companyFilter, dateFilter, employeeFilter, monthFilter, projectFilter, statusFilter]);
 
   useEffect(() => {
     if (userData && canViewReport) {
